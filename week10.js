d8f83e41244b9467128f4dfd3bcd0cc199334472
rkvrpl <rkvrpl@icloud.com>
2023-11-12 23:33:11 +0300

    added answers

--- week10.js
+++ week10.js
@@ -2,18 +2,30 @@
 // Напишите функцию firstFunction, которая будет выводить консоль переменную first.
 let first = 8;
 
-function firstFunction() {
-	return first;
-}
+// function firstFunction() {
+// 	return first;
+// }
 
-console.log(firstFunction());
+// console.log(firstFunction());   
+
+function firstFunction() {
+	console.log(first);
+}
+firstFunction();
+// или
+!function firstFunction() {
+	console.log(first);
+}();
 
 //Задание 2
 // Напишите функцию sum, которая принимает два параметра a и b, складывает их и выводит в консоль результат.
 let a = 5;
 let b = 3;
 
-function sum(a, b) {}
+function sum(a, b) {
+	let sum = a + b; 
+	return sum;
+}
 
 console.log(sum(a, b));
 
@@ -22,7 +34,10 @@ console.log(sum(a, b));
 let x = 4;
 let y = 2;
 
-function multiply(x, y) {}
+function multiply(x, y) {
+	let multiply = x * y; 
+	return multiply;
+}
 
 console.log(multiply(x, y));
 
@@ -30,8 +45,9 @@ console.log(multiply(x, y));
 // Напишите функцию greet, которая принимает один параметр name и выводит в консоль приветствие с этим именем.
 let firstName = 'Alice';
 
-function greet(firstName) {}
-
+function greet(firstName) {
+	console.log('Hello, ' + firstName + '!')
+}
 greet(firstName);
 
 //Задание 5
@@ -39,20 +55,28 @@ greet(firstName);
 let width = 6;
 let height = 4;
 
-function calculateArea(width, height) {}
-
+function calculateArea(width, height) {
+	let areaOfRectangle = (width * height) / 2;
+	return areaOfRectangle;
+}
 console.log(calculateArea(width, height));
 
 //Задание 6
 // Напишите функцию printMessage(), которая принимает один параметр message и выводит его в консоль. Если параметр не передан, используйте значение по умолчанию: "Сообщение отсутствует".
+function printMessage(message = "Сообщение отсутствует"){ 
+	console.log(message);
+}
+printMessage('Hello');
 
 //Задание 7
 // Напишите функцию secondFunction, которая принимает два параметра: c и d, и выводит в консоль их сумму. Если значения параметров не переданы, используйте значения по умолчанию: c = 5 и d = 10.
 let c = 5;
 let d = 10;
 
-function secondFunction() {}
-
+function secondFunction(c = 5, d = 10) {
+	let sum = c + d; 
+	return sum;
+}
 console.log(secondFunction()); //Выведет 15
 //Обратите ваше внимание, что мы можем передать в функцию любые аргументы и secondFunction подставит их на место параметров соответственно, перемножит их и вернёт результат.
 console.log(secondFunction(2, 3)); //Выведет 5
@@ -62,31 +86,42 @@ console.log(secondFunction(2, 3)); //Выведет 5
 let f = 2;
 let g = 3;
 
-function thirdFunction(f = 2, g = 3) {}
-
+function thirdFunction(f = 2, g = 3) {
+	let multiply = f * g; 
+	return multiply;
+}
 console.log(thirdFunction()); //Выведет 6
-
 //Обратите ваше внимание, что мы можем передать в функцию любые аргументы и thirdFunction подставит их на место параметров соответственно (если пармаметр не задан, то он подставится по умлочанию), перемножит их и вернёт результат.
 console.log(thirdFunction(4)); //Выведет 12
 console.log(thirdFunction(5, 6)); //Выведет 30
+console.log(thirdFunction(undefined, 4));
 
 //Задание 9
 // Напишите функцию приветствия (название придумайте самостоятельно), которая принимает один параметр guestName и возвращает приветствие в формате "Привет, {guestName}!". Если параметр не передан, используйте значение по умолчанию: "Гость". Чтобы увидеть результат в консоли, вызовите функцию внутри console.log.
+function saySalutation(guestName ="Гость"  ){
+	return ('Привет, ' + guestName + '!')
+}
+console.log(saySalutation());
+let name = 'Iulia';
+console.log(saySalutation(name));
+
 
 //Задание 10
 // Создайте функцию calculateDifference с помощью Function Expression и правильно используйте её, чтобы вычесть два числа и вывести результат в консоль.
 // Ваш код
 
-const result = calculateDifference(5, 3);
-
-console.log(result); //Выведет 2
+const result = function calculateDifference(q = 5, w = 3) {
+	return  q - w;
+};
+console.log(result()); //Выведет 2
 
 //Задание 11
 // Напишите функцию calculateProduct с помощью Function Expression, которая будет принимать два параметра. Функция должна перемножить значения и вывести результат в консоль.
 
-const total = calculateProduct(5, 3);
-
-console.log(total); //Выведет 15
+const total = function calculateProduct(e = 5, r = 3){
+	return e * r;
+};
+console.log(total()); //Выведет 15
 
 //Задание 12
 // Напишите стрелочную функцию calculateSquare, которая будет принимать один параметр num. Внутри функции объявите локальную переменную result и присвойте ей значение, равное квадрату числа num. Затем верните значение переменной result из функции.
